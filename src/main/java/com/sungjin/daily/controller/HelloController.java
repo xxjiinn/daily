7a06d77542cb93999dcc037a8fa2c3a2c08f0e57
xxjiinn <22100448@handong.ac.kr>
2024-04-16 11:35:35 +0900

    update daily

--- src/main/java/com/sungjin/daily/controller/HelloController.java
+++ src/main/java/com/sungjin/daily/controller/HelloController.java
@@ -3,9 +3,13 @@ package com.sungjin.daily.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class HelloController {
+//@Controller
+@RestController
+@RequestMapping("daily")
+public class
+HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello.html";
--- src/main/java/com/sungjin/daily/service/dailyService.java
+++ src/main/java/com/sungjin/daily/service/dailyService.java
@@ -1,4 +1,7 @@
 package com.sungjin.daily.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class dailyService {
 }
