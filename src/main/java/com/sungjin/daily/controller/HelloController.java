b4d354d78483bba3d00f8de10645fe14a0eb25c5
xxjiinn <22100448@handong.ac.kr>
2024-04-18 16:51:58 +0900

    edit controller

--- src/main/java/com/sungjin/daily/controller/HelloController.java
+++ src/main/java/com/sungjin/daily/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.sungjin.daily.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,11 @@ public class
 HelloController {
     @RequestMapping("/hello")
     public String hello(){
+        return "hello.html";
+    }
+
+    @GetMapping("hi")
+    public String save(){
         return "hello.html";
     }
 }
