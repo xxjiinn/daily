1c2ea0539042928b8201ce6a9fd544ff90e33868
xxjiinn <22100448@handong.ac.kr>
2024-04-29 02:49:53 +0900

    edit

--- src/main/java/com/sungjin/daily/service/dailyService.java
+++ src/main/java/com/sungjin/daily/service/dailyService.java
@@ -1,7 +1,10 @@
 package com.sungjin.daily.service;
 
+import com.sungjin.daily.repository.dailyRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class dailyService {
+    private dailyRepository dailyRepository;
+
 }
