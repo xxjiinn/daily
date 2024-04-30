0ae41fa1b508bad57bfc1cea7a9ebb71f4165210
xxjiinn <22100448@handong.ac.kr>
2024-04-30 13:07:05 +0900

    edit

--- src/main/java/com/sungjin/daily/controller/PathVariableController.java
+++ src/main/java/com/sungjin/daily/controller/PathVariableController.java
@@ -1,8 +1,10 @@
 package com.sungjin.daily.controller;
 
+import com.sungjin.daily.service.dailyService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PathVariableController {
+    private dailyService dailyService;
 
 }
